32d54f5d30bcfe321748c952d0b7ee61db3fd94a
User <User@USER-PC>
2018-02-26 10:56:30 +0200

    Add project files.

---
+++ MileStoneClient/CommunicationLayer/Message.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MileStoneClient.CommunicationLayer
+{
+    class Message
+    {
+        Guid _id;
+        string _userName;
+        DateTime _date;
+        string _message;
+
+
+    }
+}
