b4e454743ce936419030e73e2015debbf01d1f8b
BenLerer <lererb@post.bgu.ac.il>
2018-02-26 11:16:15 +0200

    updating message class

--- MileStoneClient/CommunicationLayer/Message.cs
+++ MileStoneClient/CommunicationLayer/Message.cs
@@ -8,11 +8,71 @@ namespace MileStoneClient.CommunicationLayer
 {
     class Message
     {
-        Guid _id;
-        string _userName;
-        DateTime _date;
-        string _message;
+        private Guid _id;
+        private string _userName;
+        private DateTime _date;
+        private string _messageContent;
 
+        public Guid Id
+        {
+            get
+            {
+                return _id;
+            }
 
+            set
+            {
+                _id = value;
+            }
+        }
+
+        public string UserName
+        {
+            get
+            {
+                return _userName;
+            }
+
+            set
+            {
+                _userName = value;
+            }
+        }
+
+        public DateTime Date
+        {
+            get
+            {
+                return _date;
+            }
+
+            set
+            {
+                _date = value;
+            }
+        }
+
+        public string MessageContent
+        {
+            get
+            {
+                return _messageContent;
+            }
+
+            set
+            {
+                _messageContent = value;
+            }
+        }
+
+        public Message(Guid id, string userName, DateTime date, string messageContent)
+        {
+            this._id = id;
+            this._userName = userName;
+            this._date = date;
+            this._messageContent = messageContent;
+        }
+
+         
     }
 }
