ee9f347826d04c113423a4a9249647ad41a5ff17
BenTrigger <benlerer@hotmail.com>
2018-02-28 15:18:27 +0200

    Update Communication.cs

--- MileStoneClient/CommunicationLayer/Communication.cs
+++ MileStoneClient/CommunicationLayer/Communication.cs
@@ -18,17 +18,14 @@ namespace MileStoneClient.CommunicationLayer
             /// as an HTTP post request to server at address @url.
             /// This method parse the reserver response using JSON to object of type T2.
             /// </summary>
-            /// <typeparam name="T1">Type of the data object to send</typeparam>
-            /// <typeparam name="T2">Type of the return object</typeparam>
+            /// <typeparam name="T1">Type of the data object to send and receive</typeparam>
             /// <param name="url">address of the server</param>
-            /// <param name="user">username for authentication data</param>
-            /// <param name="token">token for authentication data</param>
-            /// <param name="item">the data item to send in the reuqest</param>
-            /// <returns>the server response parsed as T2 object in json format</returns>
-            public T2 SendPostRequest<T1, T2>(string url, string user, string token, T1 item) where T2 : class
+            /// <param name="newMsg">the data to send in the reuqest</param>
+            /// <returns>the server response parsed as T1 object in json format</returns>
+            public T1 SendPostRequest<T1>(string url, T1 newMsg) : class
             {
-                var response = SendPostRequest(url, user, token, item);
-                return response == null ? null : FromJson<T2>(response);
+                var response = SendPostRequest(url, newMsg);
+                return response == null ? null : FromJson<T1>(response);
             }
 
             /// <summary>
@@ -43,11 +40,10 @@ namespace MileStoneClient.CommunicationLayer
             /// <param name="token">token for authentication data</param>
             /// <param name="item">the data item to send in the reuqest</param>
             /// <returns>the server response</returns>
-            public string SendPostRequest<T1>(string url, string user, string token, T1 item)
+            public string SendPostRequest<T1>(string url, T1 item)
             {
-                var auth = new { user, token };
                 JObject jsonItem = JObject.FromObject(item);
-                jsonItem.Add("auth", JObject.FromObject(auth));
+                //jsonItem.Add("auth", JObject.FromObject(auth));
                 StringContent content = new StringContent(jsonItem.ToString());
                 using (var client = new HttpClient())
                 {
